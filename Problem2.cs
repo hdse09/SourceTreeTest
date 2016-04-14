38414a23174993d961f75a335ad874da0098f10a
hdse09 <hansy.solis@exelsolar.com>
2016-04-14 12:49:40 -0500

    Agregando archivos

---
+++ Problem2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceTreeTest
+{
+    class Problem2
+    {
+        public void GetPrueba()
+        {
+            Console.WriteLine("Nueva Clase");
+        }
+    }
+}
